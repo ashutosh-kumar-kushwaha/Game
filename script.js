7592c0596fa9904693167ae9f03ca745417e66fc
ashutosh-kumar-kushwaha <akkushwaha0104@gmail.com>
2022-10-03 23:55:51 +0530

    Add screen and event listeners

---
+++ script.js
@@ -0,0 +1,24 @@
+const screen = document.querySelector(".screen");
+let screenHeight = screen.offsetWidth;
+let screenWidth = screen.offsetHeight;
+let rect = screen.getBoundingClientRect();
+let screenTop = rect.top;
+let screenLeft = rect.left;
+let background1 = document.querySelector(".background-1");
+let background2 = document.querySelector(".background-2");
+let posX = 0;
+
+function moveRight(){
+    posX -=3;
+    background1.style.left = posX;
+}
+
+
+document.addEventListener("keydown", (event) => {
+    if(event.key == "ArrowRight"){
+        moveRight();
+    }
+})
+
+
+console.log(screenHeight, screenWidth, screenTop, screenLeft);

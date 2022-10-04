5e4fb1b6b8ad10ba24268a2b28e5018c0c8e0979
ashutosh-kumar-kushwaha <akkushwaha0104@gmail.com>
2022-10-04 19:11:40 +0530

    Add Player and Jump

--- script.js
+++ script.js
@@ -11,13 +11,52 @@ backgroundLayer1.src = "images/bg.png";
 const backgroundLayer2 = new Image();
 backgroundLayer2.src = "images/bg.png";
 
+let playerY = 0.812 * canvasHeight;
+
+const player = new Image();
+player.src = "images/player1.png";
+
 let background1X = 0;
 let background2X = canvasWidth;
 
+let animatePlyr = 2;
+function animatePlayer(){
+    if(animatePlyr++/2 == 1){
+        player.src = "images/player1.png";
+    }
+    else if(animatePlyr++/2 == 2){
+        player.src = "images/player2.png";
+    }
+    else if(animatePlyr++/2 == 3){
+        player.src = "images/player3.png";
+    }
+    else if(animatePlyr++/2 == 4){
+        player.src = "images/player4.png";
+    }
+    else if(animatePlyr++/2 == 5){
+        player.src = "images/player5.png";
+    }
+    else if(animatePlyr++/2 == 6){
+        player.src = "images/player6.png";
+    }
+    else if(animatePlyr++/2 == 7){
+        player.src = "images/player7.png";
+    }
+    else if(animatePlyr++/2 == 8){
+        player.src = "images/player8.png";   
+    }
+    else{
+        animatePlyr = 2;
+    }
+}
+
+
+let jumpDistance = 0;
 function animate(){
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
     ctx.drawImage(backgroundLayer1, background1X, 0, canvasWidth, canvasHeight);
     ctx.drawImage(backgroundLayer2, background2X, 0, canvasWidth, canvasHeight);
+    ctx.drawImage(player, 20, playerY-100 - jumpDistance, 100, 100);
     requestAnimationFrame(animate);
 }
 
@@ -33,6 +72,7 @@ function moveRight(){
     if(background2X < -canvasWidth){
         background2X = background1X + canvasWidth;
     }
+    animatePlayer();
 }
 
 function moveLeft(){
@@ -44,6 +84,29 @@ function moveLeft(){
     if(background2X > canvasWidth){
         background2X = background1X - canvasWidth;
     }
+    animatePlayer();
+}
+
+let jumpDirection = "up";
+
+let jump;
+
+function jump2(){
+    if(jumpDirection == "up"){
+        jumpDistance += 8;
+    }
+    else{
+        jumpDistance -= 8;
+    }
+    if(jumpDistance <= 0){
+        clearInterval(jump);
+        jumpDistance = 0;
+    }
+    if(jumpDistance >= 80){
+        jumpDirection = "down";
+        jumpDistance = 80;
+    }
+    console.log(jumpDistance);
 }
 
 document.addEventListener("keydown", (event) => {
@@ -53,4 +116,15 @@ document.addEventListener("keydown", (event) => {
     if(event.key == "ArrowLeft"){
         moveLeft();
     }
-})
+    if(event.shiftKey){
+        jump = setInterval(jump2, 25);
+        jumpDirection = "up";
+    }
+});
+
+// document.addEventListener("keyup", (event) => {
+//     if(event.key == "ArrowRight" || event.key == "ArrowLeft"){
+//         clearInterval(animatePlayer);
+//     }
+// })
+
